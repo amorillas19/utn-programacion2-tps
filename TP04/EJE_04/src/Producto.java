cf735f77f1478f2b6170c145014af3f25a5230cf
Andres Morillas <60206580+amorillas19@users.noreply.github.com>
2026-04-09 21:53:07 -0300

    tp04 eje04

--- TP04/EJE_04/src/Producto.java
+++ TP04/EJE_04/src/Producto.java
@@ -1,3 +1,52 @@
 public class Producto {
+    private String nombre;
+    private double precioBase;
+    private static double IVA = 0.21;
+
+    Producto (String nombre, double precioBase){
+        setNombre(nombre);
+        setPrecioBase(precioBase);
+    }
+
+    Producto (String nombre) {
+        this(nombre, 100.00);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if (nombre == null || nombre.isEmpty()) {
+            System.out.println("El nombre no puede estar vacio");
+            return;
+        }
+            this.nombre = nombre;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+
+    public void setPrecioBase(double precioBase) {
+        if (precioBase <= 0) {
+            System.out.println("el precio no puede ser negativo");
+            return;
+        }
+        this.precioBase = precioBase;
+    }
+
+    public static double getIVA() {
+        return IVA;
+    }
+
+    public void aplicarDescuento (double descuento) {
+        setPrecioBase(getPrecioBase()*descuento);
+    }
+
+    public void aplicarDescuento (double descuento, double precioMinimo)
+
+    
+
 
 }
